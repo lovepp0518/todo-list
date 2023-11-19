f63ee309782d3ef8b93beb4d1d043d9aaef146ba
lovepp0518 <lovepp0518@gmail.com>
2023-11-19 15:34:52 +0800

    implement creating todo

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+// 要使用 express.urlencoded 來從請求網址中獲取表單資料，否則就會回傳 undefined
+app.use(express.urlencoded({ extended: true }))
+
 // 設置根路由
 app.get('/', (req, res) => {
   res.render('index');
@@ -26,11 +29,17 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/new', (req, res) => {
-  res.send('create todo')
+  res.render('new')
 })
 
 app.post('/todos', (req, res) => {
-  res.send('add todo')
+  // 取得input內容，req.body.name是在input中設置的"name"
+  const name = req.body.name
+  // 當屬性名稱與變數名稱相同時，原為{ name: name }，可寫成{ name }
+  // Todo.create是非同步，因此需要then在處理完成後執行後續程式
+  return Todo.create({ name })
+    .then(() => res.redirect('/todos'))
+    .catch((err) => console.log(err))
 })
 
 app.get('/todos/:id', (req, res) => {
