fe71f772b16c22afaaea4a124a213dc4b6486202
lovepp0518 <lovepp0518@gmail.com>
2023-11-24 16:53:16 +0800

    add flash messages

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override'); // 使用 method override
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express();
 const port = 3000
 
@@ -16,6 +18,12 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }))
 // 使用 method override 以在表單使用PUT method(表單預設僅能使用GET&POST)
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsSecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
 
 // 設置根路由
 app.get('/', (req, res) => {
@@ -27,7 +35,8 @@ app.get('/todos', (req, res) => {
     attributes: ['id', 'name', 'isComplete'],
     raw: true
   })
-    .then((todos) => res.render('todos', { todos }))
+    // 可用req.flash('success')取出參數傳入hbs
+    .then((todos) => res.render('todos', { todos, message: req.flash('success') }))
     .catch((err) => res.status(422).json(err))
 })
 
@@ -41,7 +50,12 @@ app.post('/todos', (req, res) => {
   // 當屬性名稱與變數名稱相同時，原為{ name: name }，可寫成{ name }
   // Todo.create是非同步，因此需要then在處理完成後執行後續程式
   return Todo.create({ name })
-    .then(() => res.redirect('/todos'))
+    .then(() => {
+      // 用req.flash('key', 'value')來儲存結果，key跟value可以自定義
+      req.flash('success', '新增成功!')
+      // 新增後會導向 '/todos'，因此到該頁面取出flash
+      return res.redirect('/todos')
+    })
     .catch((err) => console.log(err))
 })
 
@@ -51,7 +65,7 @@ app.get('/todos/:id', (req, res) => {
     attributes: ['id', 'name'],
     raw: true
   })
-    .then((todo) => res.render('todo', { todo }))
+    .then((todo) => res.render('todo', { todo, message: req.flash('success') }))
     .catch((err) => console.log(err))
 })
 
@@ -70,14 +84,20 @@ app.put('/todos/:id', (req, res) => {
   const id = req.params.id
 
   return Todo.update({ name, isComplete: isComplete === 'completed' }, { where: { id } })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => {
+      req.flash('success', '編輯成功!')
+      return res.redirect(`/todos/${id}`)
+    })
 })
 
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
 
   return Todo.destroy({ where: { id } })
-    .then(() => res.redirect('/todos'))
+    .then(() => {
+      req.flash('success', '刪除成功!')
+      return res.redirect('/todos')
+    })
 })
 
 // 啟動伺服器
